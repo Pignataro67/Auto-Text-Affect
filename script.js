89db07ea33b0f8372bb370e577f067ed4c0252f5
Pignataro67 <Pignataro67@gmail.com>
2021-10-15 05:55:57 -0700

    adding JS

--- script.js
+++ script.js
@@ -3,3 +3,11 @@ const speedEl = document.getElementById('speed')
 const text = 'We Love Programming!'
 let idx = 1
 let speed = 300 / speedEl.value
+
+writeText()
+
+function writeText() {
+    textEl.innerText = text.slice(0, idx)
+
+    setTimeout(writeText, speed)
+}
